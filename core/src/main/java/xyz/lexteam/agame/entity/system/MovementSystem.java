f8f9b3d6d7a4c74fa1ee62e74cb07762440c6d23
charlesstewart <tomdr1001@outlook.com>
2016-09-17 16:54:48 +0100

    Javadocs systems

--- core/src/main/java/xyz/lexteam/agame/entity/system/MovementSystem.java
+++ core/src/main/java/xyz/lexteam/agame/entity/system/MovementSystem.java
@@ -4,19 +4,37 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import xyz.lexteam.agame.component.PositionComponent;
-import xyz.lexteam.agame.component.VelocityComponent;
+import xyz.lexteam.agame.entity.component.PositionComponent;
+import xyz.lexteam.agame.entity.component.VelocityComponent;
 
-public class MovementSystem extends IteratingSystem{
+/**
+ * An {@link IteratingSystem} for changing an entity's position by a given velocity (movement).
+ */
+public class MovementSystem extends IteratingSystem {
 
+    /**
+     * Allows access to any entity's {@link PositionComponent}.
+     */
     private ComponentMapper<PositionComponent> positionMap = ComponentMapper.getFor(PositionComponent.class);
+
+    /**
+     * Allows access to any entity's {@link VelocityComponent}.
+     */
     private ComponentMapper<VelocityComponent> velocityMap = ComponentMapper.getFor(VelocityComponent.class);
 
-
+    /**
+     * Creates a new instance of {@link MovementSystem} with the correct family of components.
+     */
     public MovementSystem() {
         super(Family.all(PositionComponent.class, VelocityComponent.class).get());
     }
 
+    /**
+     * Process all entity's affected by this system.
+     *
+     * @param entity the entity to be processed
+     * @param deltaTime the time between this processing loop and the last
+     */
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         positionMap.get(entity).setX(positionMap.get(entity).getX() + velocityMap.get(entity).getX());
