6488764f24e9a1a7d784ee3286b83c4f5adf8450
Ethan <riley787cmplex@gmail.com>
2016-09-17 15:31:14 +0100

    basic input

--- core/src/main/java/xyz/lexteam/agame/AGame.java
+++ core/src/main/java/xyz/lexteam/agame/AGame.java
@@ -18,18 +18,47 @@ package xyz.lexteam.agame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Sprite sprite;
+
+    public void move(int key, boolean moving){
+
+    }
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(new Texture("player.png"));
+        Gdx.input.setInputProcessor(new InputAdapter(){
+
+            @Override
+            public boolean keyDown(int key) {
+                switch(key){
+                    case Input.Keys.W:
+                        sprite.setPosition(sprite.getX(), sprite.getY() + 1);
+                        break;
+                    case Input.Keys.S:
+                        sprite.setPosition(sprite.getX(), sprite.getY() - 1);
+                        break;
+                    case Input.Keys.A:
+                        sprite.setPosition(sprite.getX() - 1, sprite.getY());
+                        break;
+                    case Input.Keys.D:
+                        sprite.setPosition(sprite.getX() + 1, sprite.getY());
+                        break;
+                }
+                return super.keyDown(key);
+            }
+        });
 	}
 
 	@Override
@@ -37,7 +66,7 @@ public class AGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 }
